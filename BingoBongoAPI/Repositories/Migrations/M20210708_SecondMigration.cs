91ffe77ec20c546a3791a8a941a65b7e51ccd8cc
gvidassniolis <gvidas.sniolis@mediapark.com>
2021-08-07 12:11:58 +0300

    add user type

---
+++ BingoBongoAPI/Repositories/Migrations/M20210708_SecondMigration.cs
@@ -0,0 +1,27 @@
+﻿using BingoBongoAPI.Entities;
+using FluentMigrator;
+using FluentMigrator.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BingoBongoAPI.Repositories.Migrations
+{
+    [Migration(202107201046)]
+    public class M20210708_SecondMigration : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(nameof(User))
+                .AddColumn(nameof(User.Type)).AsInt16();
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
